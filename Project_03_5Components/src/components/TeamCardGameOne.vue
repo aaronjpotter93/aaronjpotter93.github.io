5cf5f9233aeb273a03e86880898ef7f933ee089a
aaronjpotter93 <81728362+aaronjpotter93@users.noreply.github.com>
2021-10-15 22:00:05 -0600

    Add files via upload

---
+++ Project_03_5Components/src/components/TeamCardGameOne.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container card">
+    <div class="row">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-3">
+            <p>5 AM</p>
+            <p>APPROX</p>
+          </div>
+          <div class="col-3">
+            <p>FPX</p>
+            <p>DK</p>
+          </div>
+          <div class="col-3">
+            <show-button/>
+            <p id="score"></p>
+          </div>
+          <div class="col-3">
+            <p>Worlds</p>
+          </div>              
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShowButton from './ShowButton.vue';
+
+
+export default {
+  name: 'TeamCardGameOne',
+  components: {
+    ShowButton
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  color:azure;
+  border-radius: 10px;
+  background-color: #313133;
+  margin-top: 20px;
+}
+</style>
