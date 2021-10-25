0ff1f63b33acc51e94f1d8ba9680fe89df9904cf
aaronjpotter93 <81728362+aaronjpotter93@users.noreply.github.com>
2021-10-24 23:03:06 -0600

    Add files via upload

---
+++ project-04/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="menu">
+</template>
+
+<script>
+import Navbar from './Navbar.vue';
+
+
+export default {
+  name: 'Navbar',
+  components: {
+    Navbar
+  }
+};
+</script>
---
+++ project-04/src/components/TeamCardGameThree.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container card">
+    <div class="row">
+      <div class="col-12">
+        <div class="row">
+          <div class="col-3">
+            <p>7 AM</p>
+            <p>APPROX</p>
+          </div>
+          <div class="col-3">
+            <p>FPX</p>
+            <p>C9</p>
+          </div>
+          <div class="col-3">
+            <show-button/>
+          </div>
+          <div class="col-3">
+            <p>Worlds</p>
+          </div>              
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShowButton from './ShowButton.vue';
+
+
+export default {
+  name: 'TeamCardGameThree',
+  components: {
+    ShowButton
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  color:azure;
+  border-radius: 10px;
+  background-color: #313133;
+  margin-top: 20px;
+}
+</style>
