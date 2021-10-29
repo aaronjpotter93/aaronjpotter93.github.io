9ef4163be2bf5c5625a69f57a1cb41865dfcf817
aaronjpotter93 <81728362+aaronjpotter93@users.noreply.github.com>
2021-10-28 23:16:04 -0600

    Add files via upload

---
+++ project_05/src/AnotherPage.js
@@ -0,0 +1,11 @@
+const AnotherPage = () => {
+    const title = 'Here is Another Page'
+
+    return ( 
+        <div className="AnotherPage">
+            <h2>{ title }</h2>
+        </div>
+     );
+}
+ 
+export default AnotherPage;
---
+++ project_05/src/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    const title = 'Welcome Project 5 created with React'
+
+    return ( 
+        <div className="home">
+            <h2>{ title }</h2>
+        </div>
+     );
+}
+ 
+export default Home;
